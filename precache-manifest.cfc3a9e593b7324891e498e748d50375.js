0a3cb7d7259badc298e3c3d8c5ce27647075721a
Thi Nguyen <tttn.tn13@gmail.com>
2021-12-28 20:47:44 -0800

    Updates

---
+++ precache-manifest.cfc3a9e593b7324891e498e748d50375.js
@@ -0,0 +1,102 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a34b8a248a46ebd0d51c2fff05ea76d",
+    "url": "/developerFolio/index.html"
+  },
+  {
+    "revision": "56f9339938444e764952",
+    "url": "/developerFolio/static/css/3.df879eac.chunk.css"
+  },
+  {
+    "revision": "b851cae481ebdf3c3e3d",
+    "url": "/developerFolio/static/css/4.e91a4b36.chunk.css"
+  },
+  {
+    "revision": "243364f8787b2daad19d",
+    "url": "/developerFolio/static/css/main.dcf76762.chunk.css"
+  },
+  {
+    "revision": "9345bd8867fb136d579f",
+    "url": "/developerFolio/static/js/2.924c71fa.chunk.js"
+  },
+  {
+    "revision": "6e1d2efc12a1ac972ff1f942df7eb0bd",
+    "url": "/developerFolio/static/js/2.924c71fa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "56f9339938444e764952",
+    "url": "/developerFolio/static/js/3.36e27f0d.chunk.js"
+  },
+  {
+    "revision": "b851cae481ebdf3c3e3d",
+    "url": "/developerFolio/static/js/4.9d05cdf4.chunk.js"
+  },
+  {
+    "revision": "243364f8787b2daad19d",
+    "url": "/developerFolio/static/js/main.9fe44be7.chunk.js"
+  },
+  {
+    "revision": "5e23bf56313b820e0723",
+    "url": "/developerFolio/static/js/runtime-main.999128bd.js"
+  },
+  {
+    "revision": "21f233e19402cc4a66866a7f31191f0d",
+    "url": "/developerFolio/static/media/Agustina.21f233e1.woff"
+  },
+  {
+    "revision": "ee6539921d713482b8ccd4d0d23961bb",
+    "url": "/developerFolio/static/media/Montserrat-Regular.ee653992.ttf"
+  },
+  {
+    "revision": "c0f806c2869eba58244b0882d543a62a",
+    "url": "/developerFolio/static/media/MySocialLogo.c0f806c2.png"
+  },
+  {
+    "revision": "3e114a26113a9351a1b43c3f7869f150",
+    "url": "/developerFolio/static/media/PathFinderLogo.3e114a26.png"
+  },
+  {
+    "revision": "77dd7faa52143f123d6dfb41febc1bbe",
+    "url": "/developerFolio/static/media/SortingVisualizer.77dd7faa.png"
+  },
+  {
+    "revision": "c59720d63dc3595a12e1c7e641a1bfa5",
+    "url": "/developerFolio/static/media/airbnbLogo.c59720d6.png"
+  },
+  {
+    "revision": "47a7a717679b640ceb1b05d38c480a61",
+    "url": "/developerFolio/static/media/codeInLogo.47a7a717.webp"
+  },
+  {
+    "revision": "51cf7192327488da3d2a171280680c16",
+    "url": "/developerFolio/static/media/contactMailDark.51cf7192.svg"
+  },
+  {
+    "revision": "59389695208d7454c6607bed51ed4316",
+    "url": "/developerFolio/static/media/developerActivity.59389695.svg"
+  },
+  {
+    "revision": "8ddf76a14a2e3ad3ba62b46d49a75a74",
+    "url": "/developerFolio/static/media/facebookLogo.8ddf76a1.png"
+  },
+  {
+    "revision": "e79c639294c805688be731921368c8f8",
+    "url": "/developerFolio/static/media/googleAssistantLogo.e79c6392.webp"
+  },
+  {
+    "revision": "52be31441c8cf8a8a16bd7a228d428bf",
+    "url": "/developerFolio/static/media/manOnTable.52be3144.svg"
+  },
+  {
+    "revision": "4b2938b9f5ea2f1ddbd45dccaff628bc",
+    "url": "/developerFolio/static/media/quoraLogo.4b2938b9.png"
+  },
+  {
+    "revision": "3440939881da8339d1597e8d77cdf850",
+    "url": "/developerFolio/static/media/skill.34409398.svg"
+  },
+  {
+    "revision": "13780cdd0144cacdef25486e2c2503f8",
+    "url": "/developerFolio/static/media/talksCardBack.13780cdd.svg"
+  }
+]);
