ebc5d83b01ec5558b824c65fc4734e845837893a
tttn13 <72048222+tttn13@users.noreply.github.com>
2021-12-30 07:02:47 +0000

    Deploying to gh-pages from  @ be02392d5fbfa75b5c6efa8472d0c55874410583 🚀

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/developerFolio/precache-manifest.044878989302b51f108c1261c2e4d0fb.js"
+  "/developerFolio/precache-manifest.6fa15530ec7fc0f41725469036a4c656.js"
 );
 
 self.addEventListener('message', (event) => {
