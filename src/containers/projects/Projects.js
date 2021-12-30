14f4d89e4bf641e507309d618a10fed3d43c135c
Thi Nguyen <tttn.tn13@gmail.com>
2021-12-29 20:34:13 -0800

    remove repo name from path

--- src/containers/projects/Projects.js
+++ src/containers/projects/Projects.js
@@ -16,7 +16,7 @@ export default function Projects() {
 
   useEffect(() => {
     const getRepoData = () => {
-      fetch("/developerFolio/profile.json")
+      fetch("/profile.json")
         .then(result => {
           if (result.ok) {
             return result.json();
